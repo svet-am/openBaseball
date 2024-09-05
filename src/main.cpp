63b27ebb935bc6244a589909b2bddae9f0047d33
Tony McDowell <svet.am@gmail.com>
2024-09-04 20:49:10 -0600

    tests: add test main.cpp as a test payload

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+//main.cpp for OpenBaseball
+//https://github.com/svet-am/openBaseball
+//written by Tony McDowell (svet.am@gmail.com)
+
+#include <iostream>
+#include "../include/obb_version.hpp"
+
+int main() {
+    std::cout << "Hello World!";
+	std::cout << "Test for " << OBB_NAME << " " << OBB_VERSION_MAJOR << "." << OBB_VERSION_MINOR << "." << OBB_VERSION_PATCH;
+    return 0;
+}
