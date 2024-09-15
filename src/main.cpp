147be1113e87d2f0a48dc64d025b75d2d81508e4
Tony McDowell <svet.am@gmail.com>
2024-09-14 19:12:34 -0600

    main: convert main.cpp to OpenGL demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,47 @@
-//main.cpp for OpenBaseball
-//https://github.com/svet-am/openBaseball
-//written by Tony McDowell (svet.am@gmail.com)
+#include "SFML/Window.hpp"
+#include "SFML/OpenGL.hpp"
 
-#include <iostream>
-#include "../include/obb_version.hpp"
+int main()
+{
+    // create the window
+    sf::Window window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default, sf::ContextSettings(32));
+    window.setVerticalSyncEnabled(true);
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
-	std::cout << "Test for " << OBB_NAME << " " << OBB_VERSION_MAJOR << "." << OBB_VERSION_MINOR << "." << OBB_VERSION_PATCH << std::endl;
+    // activate the window
+    window.setActive(true);
+
+    // load resources, initialize the OpenGL states, ...
+
+    // run the main loop
+    bool running = true;
+    while (running)
+    {
+        // handle events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                // end the program
+                running = false;
+            }
+            else if (event.type == sf::Event::Resized)
+            {
+                // adjust the viewport when the window is resized
+                glViewport(0, 0, event.size.width, event.size.height);
+            }
+        }
+
+        // clear the buffers
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // draw...
+
+        // end the current frame (internally swaps the front and back buffers)
+        window.display();
+    }
+
+    // release resources...
+
     return 0;
 }
