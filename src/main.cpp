ffcf4f6f1c311bce0ac028e4f0241ac7cab68bcf
Tony McDowell <svet.am@gmail.com>
2024-09-22 14:36:27 -0600

    main: convert hello_world to wxWidgets base rather than SFML includes refactor of main CMakeLists.txt to compile everything as static library for simplicity

--- src/main.cpp
+++ src/main.cpp
@@ -7,69 +7,81 @@
 #include "SFML/Window.hpp"
 #include "SFML/OpenGL.hpp"
 #include "SFML/Graphics.hpp"
+#include "WX/WX.h"
 #include "obb_version.hpp"
 #include "obb_utils.hpp"
 
 using namespace std;
- 
-int main(int argc, char *argv[])
+
+class MyApp : public wxApp
 {
-	//get current path of the executable
-	string obbPathString = getBasePath(&argv[0]);
-	
-	//create the splash screen window
-	sf::RenderWindow splashWindow(sf::VideoMode(512,512), "Splash Screen", sf::Style::None);
-	sf::Texture splashScreenLogoTexture;
-	
-	if (!splashScreenLogoTexture.loadFromFile(obbPathString.append("/img/obb_logo.png"), sf::IntRect(0, 0, 512, 512)))
-		return -1;
-
-	sf::Sprite splashScreenLogoSprite(splashScreenLogoTexture);	
-	splashWindow.draw(splashScreenLogoSprite);
-    splashWindow.display();		
-	Sleep(5000);	
-	splashWindow.close();
-	
-	
-    // create the main window
-    sf::Window glWindow(sf::VideoMode((sf::VideoMode::getDesktopMode().width)*.75, (sf::VideoMode::getDesktopMode().height)*.75), OBB_NAME_VER, sf::Style::Default, sf::ContextSettings(24));
-    glWindow.setVerticalSyncEnabled(true);
-
-    // activate the window
-    glWindow.setActive(true);
-
-    // load resources, initialize the OpenGL states, ...
-
-    // run the main loop
-    bool running = true;
-    while (running)
-    {
-        // handle events
-        sf::Event event;
-        while (glWindow.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                // end the program
-                running = false;
-            }
-            else if (event.type == sf::Event::Resized)
-            {
-                // adjust the viewport when the window is resized
-                glViewport(0, 0, event.size.width, event.size.height);
-            }
-        }
-
-        // clear the buffers
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        // draw...
-
-        // end the current frame (internally swaps the front and back buffers)
-        glWindow.display();
-    }
-
-    // release resources...
-
-    return 0;
+public:
+    bool OnInit() override;
+};
+ 
+wxIMPLEMENT_APP(MyApp);
+ 
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame();
+ 
+private:
+    void OnHello(wxCommandEvent& event);
+    void OnExit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+};
+ 
+enum
+{
+    ID_Hello = 1
+};
+ 
+bool MyApp::OnInit()
+{
+    MyFrame *frame = new MyFrame();
+    frame->Show(true);
+    return true;
+}
+ 
+MyFrame::MyFrame()
+    : wxFrame(nullptr, wxID_ANY, "Hello World")
+{
+    wxMenu *menuFile = new wxMenu;
+    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
+                     "Help string shown in status bar for this menu item");
+    menuFile->AppendSeparator();
+    menuFile->Append(wxID_EXIT);
+ 
+    wxMenu *menuHelp = new wxMenu;
+    menuHelp->Append(wxID_ABOUT);
+ 
+    wxMenuBar *menuBar = new wxMenuBar;
+    menuBar->Append(menuFile, "&File");
+    menuBar->Append(menuHelp, "&Help");
+ 
+    SetMenuBar( menuBar );
+ 
+    CreateStatusBar();
+    SetStatusText("Welcome to wxWidgets!");
+ 
+    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
+    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
+    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+}
+ 
+void MyFrame::OnExit(wxCommandEvent& event)
+{
+    Close(true);
+}
+ 
+void MyFrame::OnAbout(wxCommandEvent& event)
+{
+    wxMessageBox("This is a wxWidgets Hello World example",
+                 "About Hello World", wxOK | wxICON_INFORMATION);
+}
+ 
+void MyFrame::OnHello(wxCommandEvent& event)
+{
+    wxLogMessage("Hello world from wxWidgets!");
 }
