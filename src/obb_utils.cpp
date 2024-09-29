332b566464dd45de237619dff8b9c5bb73a9e615
Tony McDowell <svet.am@gmail.com>
2024-09-28 19:22:08 -0600

    obb-utils: remove redundant getBasePath() use only native wxWidgets methods

--- src/obb_utils.cpp
+++ src/obb_utils.cpp
@@ -30,38 +30,3 @@
     #define PLATFORM_NAME NULL
 #endif
 
-// Return a name of platform, if determined, otherwise - an empty string
-// Referenced from https://www.linuxquestions.org/questions/programming-9/c-function-to-get-application-path-335442/
-std::string getBasePath(char *path[]){
-	char abs_path[PATH_MAX];
-	char path_save[PATH_MAX];
-	char *p;
-
-	//check for Windows path
-	#if defined(_WIN32) || defined(_WIN64)
-	if(!(p = strrchr(*path, '\\'))){
-		
-	//check for Linux / UNIX path	
-	#elif defined(__linux__)
-	if(!(p = strrchr(*path, '\\'))){
-	#endif
-    
-	return getcwd(abs_path, sizeof(abs_path));
-  }
-  else{
-    *p = '\0';
-    getcwd(path_save, sizeof(path_save));
-    chdir(*path);
-    getcwd(abs_path, sizeof(abs_path));
-    chdir(path_save);
-	
-	return abs_path;
-  }
-  
-  
-	//Apple implementation of path fetching
-	//  #elif defined(__APPLE__) && defined(__MACH__)
-
-	// should never get here
-	return NULL;
-}
